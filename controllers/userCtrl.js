04d9c6f948324ce2dc33fbb2c40d39a475652d9d
Muhtasim <muhtasimfuad625@gmail.com>
2023-06-22 18:58:46 +0600

    twentysecondeve

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -8,16 +8,42 @@ const refreshGenerator = require('../utils/refreshGenerator')
 const userCtrl = {
     register: async (req, res) => {
         let { name, guardName, email, password } = req.body;
-        // if(password.length < 6) return res.status(400).json({msg: "password length should be at least 6"})
 
-        console.log({
-            name,
-            guardName,
-            email,
-            password
-        });
+        let dataSample = { name: name, guardName: guardName, email: email }
+
+        let errorSample = { errorCode: 0, errorMessage: "" };
+        let sample = { error: [errorSample], data: [dataSample] };
+
+        let i = 0;
+
+        if (name.length == 0) {
+            sample.error[i] = { errorCode: 101, errorMessage: "Your Name required..." }
+            i++;
+        }
+
+        if (!guardName) {
+            sample.error[i] = { errorCode: 102, errorMessage: "Guardian Name required..." }
+            i++;
+        }
+
+        if (email.length == 0) {
+            sample.error[i] = { errorCode: 103, errorMessage: "Email required..." }
+            i++;
+        }
+
+        if (!password || password.length < 6) {
+            sample.error[i] = { errorCode: 104, errorMessage: "Password length should be atleast 6..." }
+            i++;
+        }
+
+        console.log(
+            "test",
+            req.body
+        );
 
         let errors = [];
+
+        if (sample.error[0].errorCode !== 0) return res.json({ sample })
 
 
         pool.query(`SELECT * FROM userinfos where email = $1`, [email], async (err, results) => {
@@ -25,19 +51,9 @@ const userCtrl = {
 
             const passwordhash = await bcrypt.hash(password, 10)
 
-            let dataSample = { name: name, guardName: guardName, email: email, password: passwordhash }
-
-            // if(password.length < 6) return res.status(400).json({msg: "password length should be at least 6"})
-            // if(results.rows.length > 0) return res.status(400).json({msg: "the email already exist"})
-
-            let errorSample = { errorCode: 0, errorMessage: "" };
-            let sample = { error: [errorSample], data: [dataSample] };
-
-            if (password.length < 6) {
-                sample.error[0] = { errorCode: 200, errorMessage: "password length should be at least 6" }
-            }
             if (results.rows.length > 0) {
-                sample.error[1] = { errorCode: 201, errorMessage: "the email already exist" }
+                sample.error[i] = { errorCode: 105, errorMessage: "the email already exist" }
+                return res.json({ sample })
             }
 
 
@@ -58,39 +74,64 @@ const userCtrl = {
 
                 res.json({ sample, accesstoken, refreshtoken })
 
-                // res.json({password, passwordhash})
+
             })
 
 
         })
+
 
     },
     login: async (req, res) => {
         try {
             let { email, password } = req.body;
 
+            let dataSample = { email: email }
+            let errorSample = { errorCode: 0, errorMessage: "" };
+            let sample = { error: [errorSample], data: [dataSample] };
+
+            let i = 0;
+
+            if (email.length == 0) {
+                sample.error[i] = { errorCode: 103, errorMessage: "Email required..." }
+                i++;
+            }
+            if (!password || password.length < 6) {
+                sample.error[i] = { errorCode: 104, errorMessage: "Password length should be atleast 6..." }
+                i++;
+            }
+
+            console.log(
+                "test",
+                req.body
+            );
+
+            if (sample.error[0].errorCode !== 0) return res.json({ sample })
+
+
+
             pool.query(`SELECT * FROM userinfos where email = $1`, [email], async (err, results) => {
                 if (err) throw err;
 
-                let dataSample = { email: email, passwords: password }
-                let errorSample = { errorCode: 0, errorMessage: "" };
-                let sample = { error: [errorSample], data: [dataSample] };
-
                 if (results.rows.length == 0) {
-                    sample.error[0] = { errorCode: 202, errorMessage: "the user does not exist" }
-
+                    sample.error[i] = { errorCode: 106, errorMessage: "the user does not exist" }
+                    i++;
                     return res.json({ sample })
-                } else {
-                    sample.error[0] = { errorCode: "Sucess", errorMessage: "the user exist" }
+                } 
+                else {
+                    sample.error[i] = { errorCode: "No Error", errorMessage: "the user exist" }
+                    i++;
                 }
 
                 const isMatch = await bcrypt.compare(password, results.rows[0].password);
 
                 if (!isMatch) {
-                    sample.error[1] = { errorCode: 203, errorMessage: "incorrect password" }
+                    sample.error[i] = { errorCode: 107, errorMessage: "incorrect password" }
+                    i++;
+                    return res.json({ sample })
                 }
 
-                // res.json({ msg: "Successful LOGIN"})
+
 
                 const accesstoken = jwtGenerator(results.rows[0].id)
                 const refreshtoken = refreshGenerator(results.rows[0].id)
